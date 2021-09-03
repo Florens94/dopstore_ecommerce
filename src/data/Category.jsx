3ae06028347ec5f9119f79b214c2b63673ec0f66
Florencia Duarte <florbelenduarte@gmail.com>
2021-09-03 13:58:50 -0300

    Update Category.jsx

--- src/data/Category.jsx
+++ src/data/Category.jsx
@@ -1,34 +1,33 @@
-// import { Fragment } from 'react'
-// import { Item } from '../components/item/item'
-// import { Page } from '../components/Page/Page'
-// import { useParams } from 'react-router-dom'
-// // import items from '../data/items.json'
-// // import styles from '../styles/pages/category.module.scss'
+import { Fragment } from 'react'
+import Item from '../components/Item/Item'
+import { useParams } from 'react-router-dom'
+import {database} from '../firebase'
 
-// const Category = () => {
-// 	const { categoryName } = useParams()
-// 	// const products = items.filter((item) => item.category === categoryName)
-//     const lentes = database.collection('lentes').where('categoria', '==', categoria)
+const Category = () => {
+	const { categoryName } = useParams()
+	const products = products.filter((item) => item.category === categoryName)
+    const lentes = database.collection('lentes').where('category', '===', categoryName)
 
-// 	return (
-// 		<Page id={`category_${categoryName}`}>
-// 			<Fragment>
-// 				<h1 className={styles.title}>
-// 					{categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}
-// 				</h1>
-// 				<div className={styles.list}>
-// 					{products.map((item) => (
-// 						<Item
-// 							key={item.name}
-// 							image={item.image}
-// 							name={item.name}
-// 							price={item.price}
-// 						/>
-// 					))}
-// 				</div>
-// 			</Fragment>
-// 		</Page>
-// 	)
-// }
+	return (
 
-// export default Category
+			<Fragment>
+				<h1>
+					{categoryName.charAt(0).toUpperCase()}
+				</h1>
+				<div>
+					{lentes.map((item) => (
+						<Item
+							key={item.name}
+							image={item.image}
+							name={item.name}
+							price={item.price}
+						/>
+					))}
+				</div>
+			</Fragment>
+
+	)
+}
+
+export default Category
+
